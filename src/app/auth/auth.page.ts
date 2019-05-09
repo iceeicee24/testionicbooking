4cfde987c78a03f14feedee45f59eeee1dfd4989
iceeicee24 <iceeicee24@hotmail.com>
2019-05-10 07:27:44 +1000

    update

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,8 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
+
   constructor(private authService: AuthService,
               private router: Router,
               private loadingCtrl: LoadingController) { }
@@ -31,6 +34,28 @@ export class AuthPage implements OnInit {
       }, 5000);
     });
     this.isLoading = true;
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    console.log(email, password);
+
+    if (this.isLogin) {
+      //send to login 
+    } else {
+      //
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
